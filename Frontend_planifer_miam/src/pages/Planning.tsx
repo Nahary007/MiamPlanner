c885ba6c807f8ccd5d551c7ab23d65ffc77c33cc
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-07-24 09:57:49 +0300

    fix/ resolve all bugg on planning page and his component

--- Frontend_planifer_miam/src/pages/Planning.tsx
+++ Frontend_planifer_miam/src/pages/Planning.tsx
@@ -37,6 +37,7 @@ const Planning: React.FC = () => {
       const data = await plannedMealsAPI.getWeek(
         format(currentWeek, "yyyy-MM-dd"),
       );
+      console.log(plannedMeals);
       // Correction ici : s'assurer que c'est bien un tableau
       if (Array.isArray(data)) {
         setPlannedMeals(data);
@@ -185,7 +186,7 @@ const Planning: React.FC = () => {
                     {meal ? (
                       <div className="bg-emerald-50 border border-emerald-200 rounded-lg p-3 group relative">
                         <div className="text-sm font-medium text-emerald-900 mb-1">
-                          {meal.recipe.name_recipe}
+                          {meal.recipe.nameRecipe}
                         </div>
                         <div className="text-xs text-emerald-700">
                           {meal.recipe.servings} portions
@@ -237,7 +238,7 @@ const Planning: React.FC = () => {
                       {meal ? (
                         <div className="bg-emerald-50 border border-emerald-200 rounded-lg p-2 group relative">
                           <div className="text-sm font-medium text-emerald-900 mb-1">
-                            {meal.recipe.name_recipe}
+                            {meal.recipe.nameRecipe}
                           </div>
                           <div className="text-xs text-emerald-700">
                             {meal.recipe.servings} portions
